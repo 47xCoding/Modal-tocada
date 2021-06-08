0f549ac63ced70cae33302aa42830f6acf8d0919
47xCoding <74483645+47xCoding@users.noreply.github.com>
2021-06-08 15:24:35 +0600

    Added scripts.
    Added scripts to open and close modal.

--- setup/app.js
+++ setup/app.js
@@ -2,3 +2,17 @@
 // listen for click events on modal-btn and close-btn
 // when user clicks modal-btn add .open-modal to modal-overlay
 // when user clicks close-btn remove .open-modal from modal-overlay
+
+// select items
+const modalBtn = document.querySelector(".modal-btn");
+const closeBtn = document.querySelector(".close-btn");
+const modalOverlay = document.querySelector(".modal-overlay");
+
+// add event
+modalBtn.addEventListener('click', function () {
+ modalOverlay.classList.add("open-modal");
+})
+
+closeBtn.addEventListener('click', function() {
+ modalOverlay.classList.remove("open-modal");
+})
